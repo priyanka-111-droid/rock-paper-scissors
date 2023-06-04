910027a00cf049d1263b940ee97f44195afb6c8d
Priyanka <priyankarajanpg@gmail.com>
2023-06-04 07:10:51 -0400

    rock paper sc done

--- script.js
+++ script.js
@@ -45,13 +45,42 @@ function playRound(playerSelection,computerSelection){
 
 function game(){
     let userSelection,computerSelection;
-    for(let i=1;i<=5;i++){
-        userSelection = prompt("Enter choice: rock/paper/scissors");
-        userSelection = userSelection.toLowerCase();
-        computerSelection = getComputerChoice();
-        console.log(`Round ${i}:${playRound(userSelection,computerSelection)}`);
-    }
-    console.log(`Winner is ${playerScore>computerScore?"User":"Computer"}`);
-}
+    // for(let i=1;i<=5;i++){
+    //     userSelection = prompt("Enter choice: rock/paper/scissors");
+    //     userSelection = userSelection.toLowerCase();
+    //     computerSelection = getComputerChoice();
+    //     console.log(`Round ${i}:${playRound(userSelection,computerSelection)}`);
+    // }
+    // console.log(`Winner is ${playerScore>computerScore?"User":"Computer"}`);
+    let i=1;
+    // while(playerScore!=5||computerScore!=5){
+        
+    const result = document.querySelector('#result');
+    const p = document.createElement('p');
+    
+        const btnList = document.querySelectorAll('button');
+        btnList.forEach((button)=>{
+            computerSelection = getComputerChoice();
+            button.addEventListener('click',function(e){
+                // playRound(e.target,computerSelection)
+                // console.log(`Round ${i}:${playRound(e.target.id,computerSelection)} :${playerScore},${computerScore}`);
+                p.textContent+=`Round ${i}:${playRound(e.target.id,computerSelection)} :${playerScore},${computerScore}\n`;
+                p.style.whiteSpace = 'pre-line';
+                result.appendChild(p);
 
+                i+=1;
+                // console.log(e.target);
+                if(playerScore==5||computerScore==5){
+                    // console.log(`Winner is ${playerScore>computerScore?"User":"Computer"}`);
+                    if(playerScore==5){
+                        p.textContent=`Winner is user`;
+                    }else{
+                        p.textContent=`Winner is computer`;
+                    }
+                    result.appendChild(p);
+                    return;
+                }
+            });
+        });
+}
 game();
