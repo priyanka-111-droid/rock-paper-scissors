daa109ee95a57bdee0d69fab98157b64108252c7
Priyanka <priyankarajanpg@gmail.com>
2023-07-09 04:15:37 -0400

    added button image to ui

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let playerScore=0;
 let computerScore=0;
+let userDisplayScore = document.querySelector('#scores #user');
+let computerDisplayScore = document.querySelector('#scores #computer');
+let userHeading = document.querySelector('#scores #user-heading');
+let computerHeading = document.querySelector('#scores #computer-heading');
+
+
+
 function getComputerChoice(){
     let choices=['rock','paper','scissors'];
     let randomIndex = Math.floor(Math.random()*choices.length);
@@ -7,40 +14,43 @@ function getComputerChoice(){
 }
 // console.log(getComputerChoice());
 function playRound(playerSelection,computerSelection){
+    let output="";
     if(playerSelection==computerSelection){
         return 'No one wins!';
     }
     else if(playerSelection=='rock'){
         if(computerSelection=='scissors'){
             playerScore+=1;
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            output = `You win! ${playerSelection} beats ${computerSelection}`;
         }
         else {
             computerScore+=1;
-            return `You loose! ${computerSelection} beats ${playerSelection}`;
+            output = `You loose! ${computerSelection} beats ${playerSelection}`;
         }
     }
     else if(playerSelection=='scissors'){
         if(computerSelection=='rock'){
             computerScore+=1;
-            return `You loose! ${computerSelection} beats ${playerSelection}`;
+            output = `You loose! ${computerSelection} beats ${playerSelection}`;
         }
         else {
             playerScore+=1;
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            output = `You win! ${playerSelection} beats ${computerSelection}`;
         }
     }
     else{
         if(computerSelection=='rock'){
             playerScore+=1;
-            return `You win! ${playerSelection} beats ${computerSelection}`;
+            output = `You win! ${playerSelection} beats ${computerSelection}`;
         }
         else{
             computerScore+=1;
-            return `You loose! ${computerSelection} beats ${playerSelection}`;
+            output = `You loose! ${computerSelection} beats ${playerSelection}`;
         }
     }
-        
+    userDisplayScore.textContent = playerScore;
+    computerDisplayScore.textContent=computerScore; 
+    return output;
 }
 
 function game(){
@@ -54,7 +64,6 @@ function game(){
     // console.log(`Winner is ${playerScore>computerScore?"User":"Computer"}`);
     let i=1;
     // while(playerScore!=5||computerScore!=5){
-        
     const result = document.querySelector('#result');
     const p = document.createElement('p');
     
@@ -63,9 +72,8 @@ function game(){
             computerSelection = getComputerChoice();
             button.addEventListener('click',function(e){
                 // playRound(e.target,computerSelection)
-                // console.log(`Round ${i}:${playRound(e.target.id,computerSelection)} :${playerScore},${computerScore}`);
-                p.textContent+=`Round ${i}:${playRound(e.target.id,computerSelection)} :${playerScore},${computerScore}\n`;
-                p.style.whiteSpace = 'pre-line';
+                p.textContent=`Round ${i}:${playRound(e.target.id,computerSelection)}\n`;
+                // p.style.whiteSpace = 'pre-line';
                 result.appendChild(p);
 
                 i+=1;
@@ -74,13 +82,16 @@ function game(){
                     // console.log(`Winner is ${playerScore>computerScore?"User":"Computer"}`);
                     if(playerScore==5){
                         p.textContent=`Winner is user`;
+                        userHeading.style.border='2px solid green';
                     }else{
                         p.textContent=`Winner is computer`;
+                        computerHeading.style.border='2px solid green';
                     }
                     result.appendChild(p);
                     return;
                 }
             });
         });
+        
 }
 game();
